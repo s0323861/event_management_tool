96e6bcdddb689c2cd380a921045f05fea8a3633f
Akira Mukai <s0323861@gmail.com>
2021-01-27 17:04:41 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,29 +1,25 @@
-$(document).ready(function () {
+$(function () {
+
     //Initialize tooltips
     $('.nav-tabs > li a[title]').tooltip();
 
     //Wizard
     $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
-
         var $target = $(e.target);
-
         if ($target.parent().hasClass('disabled')) {
             return false;
         }
     });
 
     $(".next-step").click(function (e) {
-
         var $active = $('.wizard .nav-tabs li.active');
         $active.next().removeClass('disabled');
         nextTab($active);
-
     });
-    $(".prev-step").click(function (e) {
 
+    $(".prev-step").click(function (e) {
         var $active = $('.wizard .nav-tabs li.active');
         prevTab($active);
-
     });
 
     // 入力チェック（ボタンを押させない）
@@ -31,11 +27,11 @@ $(document).ready(function () {
         $('#stp1btn').prop('disabled', true);
     }
     $('#inputName').on('keydown keyup keypress change', function() {
-    if ($(this).val().length > 0) {
-        $('#stp1btn').prop('disabled', false);
-    } else {
-        $('#stp1btn').prop('disabled', true);
-    }
+        if ($(this).val().length > 0) {
+            $('#stp1btn').prop('disabled', false);
+        } else {
+            $('#stp1btn').prop('disabled', true);
+        }
     });
 
     // 入力チェック（ボタンを押させない）
@@ -75,20 +71,15 @@ $(document).ready(function () {
             $('#result1').val('失敗');
             $('#result2').val(errorHandler(arguments));
         });
-
         var $active = $('.wizard .nav-tabs li.active');
         $active.next().removeClass('disabled');
         nextTab($active);
-
     });
-
-});
-
-$(function () {
 
     $('#datetimepicker1').datetimepicker({
         locale: 'ja',
-        format : 'M/D(dd) HH:mm'
+        format : 'YYYY/M/D(dd) HH:mm',
+        sideBySide : true
     });
 
     // 入力後、ボタンを押せるようになる
@@ -98,11 +89,13 @@ $(function () {
 
     $('#datetimepicker2').datetimepicker({
         locale: 'ja',
-        format : 'M/D(dd) HH:mm'
+        format : 'YYYY/M/D(dd) HH:mm',
+        sideBySide : true
     });
     $('#datetimepicker3').datetimepicker({
         locale: 'ja',
-        format : 'M/D(dd) HH:mm'
+        format : 'YYYY/M/D(dd) HH:mm',
+        sideBySide : true
     });
 
 });
@@ -113,4 +106,3 @@ function nextTab(elem) {
 function prevTab(elem) {
     $(elem).prev().find('a[data-toggle="tab"]').click();
 }
-
